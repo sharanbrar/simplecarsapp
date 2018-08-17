b6d372ca9544a64276bc3bf7acbdd6b0b536332d
Unknown <sharanbrar@gmail.com>
2018-08-17 20:47:53 +0530

    changes

--- src/pages/scan-license/scan-license.ts
+++ src/pages/scan-license/scan-license.ts
@@ -105,7 +105,7 @@ export class ScanLicensePage {
   getImage(){
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
